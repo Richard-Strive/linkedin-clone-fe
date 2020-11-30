1ff74346c10431f22626eeb505309ea6adb6f458
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-11-30 23:09:16 +0100

    for now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 import Profile from "./components/main_components/Profile";
-import SideComps from "./components/sideComponents/sideComps";
+import SideComps from "./components/sideComponents/SideComps";
 function App() {
   return (
     <div className="App">
