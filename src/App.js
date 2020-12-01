151f90694152e5f7d4cf0c66837c70a3820737f8
Leonabimi <72199021+Leonabimi@users.noreply.github.com>
2020-12-01 11:07:51 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 import Profile from "./components/main_components/Profile";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
-	return (
-		<div className='App'>
-			<NavBar />
-			<Profile />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Profile />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
