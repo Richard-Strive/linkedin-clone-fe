f804398d1dfcdd1be870bf9e160090b093c66566
Leonabimi <72199021+Leonabimi@users.noreply.github.com>
2020-12-02 16:18:10 +0100

    Leon

--- src/components/main_components/Home.jsx
+++ src/components/main_components/Home.jsx
@@ -1,35 +1,32 @@
 import React from "react";
 
-import {Container, Row, Col} from 'react-bootstrap'
-
+import { Container, Row, Col } from "react-bootstrap";
 
 import Posts from "../home_subcomponents/Posts";
-import MakePost from '../home_subcomponents/MakePost'
+import MakePost from "../home_subcomponents/MakePost";
+import LeftSide from "../sideComponents/LeftSide";
+import RightSide from "../sideComponents/RightSide";
 
 class Home extends React.Component {
-
-	state = {};
-	render() {
-		return (
-			<div>
-				<Container>
-					<Row>
-						<Col xs={3}>
-							left component
-						</Col>
-						<Col xs={6}>
-						<MakePost/>
-						<Posts />
-						</Col>
-						<Col xs={3}>
-							right component
-						</Col>
-					</Row>
-				</Container>
-				
-			</div>
-		);
-	}
+  state = {};
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col xs={3}>
+              <LeftSide />
+            </Col>
+            <Col xs={6}>
+              <MakePost />
+              <Posts />
+            </Col>
+            <Col xs={3}>right component</Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/sideComponents/RightSide.jsx
+++ src/components/sideComponents/RightSide.jsx
@@ -1,143 +1,19 @@
 import React, { Component } from "react";
-import { Card, Row, Col } from "react-bootstrap";
-import "../../App.css";
 
-class RightSide extends React.Component {
-  state = {
-    user: {},
-  };
-
-  fetchUser = async () => {
-    try {
-      const response = await fetch(
-        process.env.REACT_APP_BASE_URL + `profile/me`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
-          },
-        }
-      );
-
-      const user = await response.json();
-      console.log(user);
-      this.setState({ user });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  componentDidMount() {
-    this.fetchUser();
-  }
-
+class RightSide extends Component {
   render() {
     return (
-      <>
-        <Card style={{ width: "14rem", borderRadius: "10px" }}>
-          <Card.Header className="card-header">
-            <Card.Img
-              style={{
-                height: "70px",
-                width: "70px",
-                borderRadius: "50%",
-                alignSelf: "center",
-              }}
-              variant="top"
-              src={this.state.user.image}
-            />
-          </Card.Header>
-
-          <Card.Body>
-            <Card.Title>
-              Welcome,{this.state.user.name}! <br />
-              <a style={{ fontSize: "12px" }} href="/">
-                Add a Photo
-              </a>
-            </Card.Title>
-            <hr />
-            <Card.Text>
-              <Row>
-                <Col style={{ textAlign: "left" }}>
-                  <div>
-                    <div
-                      style={{
-                        color: "gray",
-                        fontSize: "13px",
-                      }}
-                    >
-                      <span>Connections</span>
-                    </div>
-                    <div>
-                      <span style={{ fontSize: "13px" }}>
-                        Grow your network
-                      </span>
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <div>
-                      <a
-                        style={{
-                          color: "gray",
-                          fontSize: "13px",
-                        }}
-                      >
-                        Access exclusive tools insights
-                      </a>
-                    </div>
-                    <div>
-                      <a style={{ fontSize: "13px" }}>
-                        <i
-                          class="fas fa-square"
-                          style={{ color: "rgb(186, 189, 11)" }}
-                        ></i>
-                        Reactivate Premium
-                      </a>
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <p style={{ fontSize: "13px" }}>
-                      <i class="fas fa-bookmark"></i>My items
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card
-          style={{ width: "14rem", borderRadius: "10px", marginTop: "8px" }}
-        >
-          <Card.Body>
-            <Card.Text>
-              <Row>
-                <Col style={{ textAlign: "left" }}>
-                  <div>
-                    <a href="/" style={{ fontSize: "13px" }}>
-                      Groups
-                    </a>
-                  </div>
-                  <div>
-                    <a href="/" style={{ fontSize: "13px" }}>
-                      Events
-                    </a>
-                  </div>
-                  <div>
-                    <a href="/" style={{ fontSize: "13px" }}>
-                      Followed Hashtags
-                    </a>
-                  </div>
-                </Col>
-              </Row>
-            </Card.Text>
-          </Card.Body>
-
-          <Card.Body className="discover">
-            <div>
-              <h6 style={{ textAlign: "center" }}>Discover more</h6>
-            </div>
-          </Card.Body>
-        </Card>
-      </>
+      <div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Add to your feed</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
