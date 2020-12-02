cb8d22450a9898a26abd1479278fe3fe2a6e4976
Leonabimi <72199021+Leonabimi@users.noreply.github.com>
2020-12-02 16:00:32 +0100

    leon

--- src/components/main_components/Home.jsx
+++ src/components/main_components/Home.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import RightSide from "../sideComponents/RightSide";
+import { Container, Row, Col } from "react-bootstrap";
 
 import Posts from "../home_subcomponents/Posts";
 
 class Home extends React.Component {
-	state = {};
-	render() {
-		return (
-			<div>
-				<Posts />
-			</div>
-		);
-	}
+  state = {};
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <RightSide />
+            </Col>
+            <Col>
+              <Posts />
+            </Col>
+            <Col>
+              <RightSide />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Home;
